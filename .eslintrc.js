07f8badb9ff5b15a2b42daa29143bd86bafbd964
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-06-20 17:04:32 +0300

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+// @ts-check
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
+    '@typescript-eslint/no-explicit-any': 'error',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+  },
+  ignorePatterns: ['dist/', '.next/', 'node_modules/'],
+};
