937613a596a032bf21965f812306cb7b32996719
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-06-21 10:58:44 +0300

    add user enums

---
+++ packages/shared/src/enums/user.enum.ts
@@ -0,0 +1,13 @@
+export enum UserRole {
+  CUSTOMER = 'customer',
+  RESTAURANT_OWNER = 'restaurant_owner',
+  RIDER = 'rider',
+  ADMIN = 'admin',
+}
+
+export enum UserStatus {
+  ACTIVE = 'active',
+  INACTIVE = 'inactive',
+  SUSPENDED = 'suspended',
+  PENDING = 'pending',
+}
